66b777e681cc76c8a0abd9285491897992fc1804
euoom <euoom.dev@gmail.com>
2025-08-30 11:51:54 +0900

    .

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ async def handle_interactions(interaction: Interaction):
 @app.get("/")
 def read_root():
     return {"message": "Knowleadge Base Backend is running."}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
